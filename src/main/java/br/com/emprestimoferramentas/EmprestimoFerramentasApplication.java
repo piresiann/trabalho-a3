4724dee4b498fc037067a83f6ff25f9c5fa2c795
Ian Pires <ianbr23@gmail.com>
2025-08-29 23:40:03 -0300

    commit inicial

---
+++ src/main/java/br/com/emprestimoferramentas/EmprestimoFerramentasApplication.java
@@ -0,0 +1,13 @@
+package br.com.emprestimoferramentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoFerramentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoFerramentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emprestimoferramentas/EmprestimoFerramentasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emprestimoferramentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoFerramentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
